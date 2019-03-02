1acd2d7a86818895ba504d8827668b9d7b0a2d7b
Rishi Sharma <rishi.sharma@gdn-commerce.com>
2019-03-02 12:19:32 +0530

    initial setup

---
+++ src/main/java/com/sih/microfinancing/MicrofinancingApplication.java
@@ -0,0 +1,13 @@
+package com.sih.microfinancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofinancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrofinancingApplication.class, args);
+	}
+
+}
