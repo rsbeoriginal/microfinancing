e1896b22dc8c02b2f6aa631d38d742fdb4d5e5bb
Rishi Sharma <rishi.sharma@gdn-commerce.com>
2019-03-02 15:57:13 +0530

    @CrossOrigin

--- src/main/java/com/sih/microfinancing/controller/TransactionController.java
+++ src/main/java/com/sih/microfinancing/controller/TransactionController.java
@@ -1,9 +1,11 @@
 package com.sih.microfinancing.controller;
 
 import com.sih.microfinancing.config.ApiConstants;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping(ApiConstants.TRANSACTION)
 public class TransactionController {
